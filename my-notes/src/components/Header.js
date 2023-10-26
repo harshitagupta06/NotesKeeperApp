6a415fc787cdd922fe0d07065c1a09c0d6b7a0b4
harvid <harvid@Harvid.local>
2023-10-26 19:40:26 +0530

    First commit

---
+++ my-notes/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <ModeEditOutlineIcon />
+            Notes Keeping App
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
